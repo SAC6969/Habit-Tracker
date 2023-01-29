24cd0493faf7ec1a0e2308880914fccba88cad2e
Sachin Varma <sachinvermaa1234@gmail.com>
2023-01-29 12:48:22 +0530

    Done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+const URL = `mongodb+srv://sac6969:sac6969@cluster0.lsq3rrg.mongodb.net/?retryWrites=true&w=majority`;
+
+// mongoose.connect('mongodb://127.0.0.1/Habit-Tracker');
+
+mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser: true});
 
 const db = mongoose.connection;
 
